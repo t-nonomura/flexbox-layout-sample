e6240e73106c8926fd4df5906e467ed1fa97f07b
t.nonomura <nonomura0812@gmail.com>
2019-06-27 23:00:21 +0900

    first commit

---
+++ app/src/main/java/net/storehouse/nono/flexboxlayoutsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.storehouse.nono.flexboxlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
